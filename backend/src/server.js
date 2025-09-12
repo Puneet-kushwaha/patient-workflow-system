945a551a0db2e99d25d8cb7b752d8b53ab3c5b12
Puneet-kushwaha <pk758992@gmail.com>
2025-09-12 19:56:48 +0530

    fix cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,39 +1,51 @@
 require("dotenv").config();
 const express = require("express");
 const fileUpload = require("express-fileupload");
-const {format, parse, union} = require("express-form-data");
+const { format, parse, union } = require("express-form-data");
 const cors = require("cors");
 const os = require("os");
 const path = require("path");
 
 const app = express();
-
 const PORT = process.env.PORT || 8001;
 
-app.use(cors());
+const allowedOrigins = [
+    "https://patient-workflow-system-frontend.onrender.com",
+    "http://localhost:5173/",
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true,
+}));
+
+app.options(/.*/, cors());
+
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
     req.body = {
         ...req.body,
-        ...req.files
+        ...req.files,
     };
-
     next();
 });
 
 app.use(parse({
     uploadDir: os.tmpdir(),
-    autoClean: true
+    autoClean: true,
 }));
 app.use(format());
 app.use(union());
 
-// app.use(fileUpload({ createParentPath: true }));
-
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
-
-app.use(express.json());
 
 const indexRoutes = require("./routes/index");
 app.use("/api", indexRoutes);
