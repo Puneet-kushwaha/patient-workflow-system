13943a9a2b4bdb5c24a9b6cfe6cd73b27b7cd4c9
Puneet-kushwaha <pk758992@gmail.com>
2025-09-12 19:41:28 +0530

    push

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    allowedHosts: [
-      "localhost",
-      "7dc8da4f8064.ngrok-free.app"
-    ],
-    port: 5173
-  }
+  plugins: [react()]
 })
